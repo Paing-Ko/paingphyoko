bb0e0699fffb31fafe1735bdda78fdede9863848
Paing Ko <paingphyoko.keke@gmail.com>
2023-08-24 14:11:48 +0100

    pokemon update

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -2,12 +2,11 @@ const pokemonInput = document.getElementById("pokemonInput");
 const showButton = document.getElementById("showButton");
 const pokemonDiv = document.getElementById("pokemonDiv");
 const nameSpan = document.getElementById("nameSpan");
+const spriteImage = document.getElementById("spriteImage");
 const heightSpan = document.getElementById("heightSpan");
 const weightSpan = document.getElementById("weightSpan");
-
-const experienceSpan = document.getElementById("experienceSpan");
-const typesSpan = document.getElementById("typesSpan");
-const spriteImg = document.getElementById("spriteImg");
+const baseExperienceSpan = document.getElementById("baseExperienceSpan");
+const typeSpan = document.getElementById("typeSpan");
 
 async function showPokemon() {
   const pokemonName = pokemonInput.value.toLowerCase();
@@ -16,16 +15,23 @@ async function showPokemon() {
   );
   const pokemon = await response.json();
   nameSpan.textContent = pokemon["name"];
+  spriteImage.src = pokemon["sprites"]["front_default"];
+  spriteImage.alt = `Front view of ${pokemon["name"]}`;
   heightSpan.textContent = pokemon["height"];
   weightSpan.textContent = pokemon["weight"];
-  experienceSpan.textContent = pokemon["base_experience"];
-
-  typesSpan.textContent = pokemon["types"]
-    .map((type) => type.type.name)
-    .join(", ");
-
-  spriteImg.src = pokemon["sprites"]["front_default"];
+  baseExperienceSpan.textContent = pokemon["base_experience"];
+  const types = [];
+  for (const typeData of pokemon["types"]) {
+    types.push(typeData["type"]["name"]);
+  }
+  typeSpan.textContent = types.join(" and ");
   pokemonDiv.hidden = false;
 }
 
 showButton.addEventListener("click", showPokemon);
+
+function rotateSpriteImage() {
+  spriteImage.classList.toggle("rotated");
+}
+
+spriteImage.addEventListener("click", rotateSpriteImage);
