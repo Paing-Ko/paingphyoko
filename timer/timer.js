173f4eb918a9d3b49d97cf8d9ecd1698cefae882
Paing Ko <paingphyoko.keke@gmail.com>
2023-09-06 16:27:16 +0100

    timer update

--- timer/timer.js
+++ timer/timer.js
@@ -1,10 +1,25 @@
+const timeInput = document.getElementById("time-input");
+const displayTime = document.getElementById("display-time");
 let interval;
 
-function startTimer() {
-  const timeInput = document.getElementById("time-input");
-  const displayTime = document.getElementById("display-time");
+// New event listener for the timeInput
+timeInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    startTimer();
+  }
+});
 
+function startTimer() {
   let timeLeft = timeInput.value;
+
+  // Check if input is not a number or less than or equal to 0
+  if (isNaN(timeLeft) || timeLeft <= 0) {
+    alert("Please enter a valid time greater than 0.");
+    return; // Exit the function
+  }
+
+  // Display the initial value
+  displayTime.textContent = timeLeft;
 
   // Clear any existing interval
   clearInterval(interval);
@@ -17,9 +32,15 @@ function startTimer() {
     if (timeLeft <= 0) {
       clearInterval(interval);
       displayTime.textContent = "0";
-
+      timeInput.value = ""; // Clear the input field
       // Stretch Requirement: Notify user
       alert("Time's up!");
     }
   }, 1000);
 }
+
+timeInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    startTimer();
+  }
+});
